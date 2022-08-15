23cd634c16561c50343da0004e2d31360ee75a15
Aftab69 <aftabhssn3@gmail.com>
2022-08-15 14:20:27 +0530

    form handling and navigation added

--- backend/Routing.js
+++ backend/Routing.js
@@ -12,19 +12,19 @@ router.post("/register",async(req,res)=>{
     try{
         const { name, email, password, cpassword } = req.body;
         if(!name || !email || !password || !cpassword){
-            res.json({"message":"Please fill your form"})
+            res.status(400).json({"message":"Please fill your form"})
         }
         const userExist = await User.findOne({email:email});
         if(userExist){
-            res.json({"message":"Email already exists"})
+            res.status(401).json({"message":"Email already exists"})
         } else if(password!==cpassword){
-            res.json({"message":"The password confirmation does not match"})
+            res.status(402).json({"message":"The password confirmation does not match"})
         } else {
             const newUser = new User({name,email,password,cpassword});
             newUser.password =await bcrypt.hash(newUser.password,12);
             newUser.cpassword =await bcrypt.hash(newUser.cpassword,12);
             await newUser.save();
-            res.json({"message":"User successfully registered"});
+            res.status(200).json({"message":"User successfully registered"});
         }
     }catch(error){
         console.log(error)
@@ -35,20 +35,20 @@ router.post("/login",async(req,res)=>{
     try{
         const { email, password } = req.body;
         if(!email || !password){
-            res.json({"message":"Please fill your form"})
+            res.status(400).json({"message":"Please fill your form"})
         }
         const userExist = await User.findOne({email:email});
         if(!userExist){
-            res.json({"message":"Invalid credentials"})
+            res.status(401).json({"message":"Invalid credentials"})
         }
         const passwordMatched =await bcrypt.compare(password,userExist.password);
         if(!passwordMatched){
-            res.json({"message":"Invalid credentials"})
+            res.status(401).json({"message":"Invalid credentials"})
         } else if(passwordMatched){
             const token = jwt.sign({_id:userExist._id},process.env.PRIVATEKEY);
             userExist.tokens.push({token: token});
             userExist.save();
-            res.json({"message":"User successfully logged in"})
+            res.status(200).json({"message":"User successfully logged in"})
         }
     }catch(error){
         console.log(error)
--- frontend/src/Register.js
+++ frontend/src/Register.js
@@ -1,21 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Register.css";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
+  let navigate = useNavigate();
+  const [ data, setData ] = useState({
+    "name":"",
+    "email":"",
+    "password":"",
+    "cpassword":""
+  })
+  const handleChange = (e) =>{
+    setData({...data,[e.target.name]:e.target.value});
+  }
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    const { name, email, password, cpassword } = data;
+    fetch("/register",{
+      method:"POST",
+      headers:{
+        "Content-Type":"application/json",
+      },
+      body:JSON.stringify({
+        name, email, password, cpassword
+      })
+    }).then((res)=>{
+      if(res.status===200){
+        alert("User successfully registered")
+        navigate("/login");
+      } else if(res.status===400){
+        alert("Please fill your form")
+      } else if(res.status===401){
+        alert("Email already exists")
+      } else if(res.status===402){
+        alert("The password confirmation does not match")
+      }
+    })
+  }
   return (
     <>
       <div className='registerpageContainer'>
-          <div className='registerinfoContainer'>
+          <form method='POST' onSubmit={handleSubmit} className='registerinfoContainer'>
               <p>Full Name :</p>
-              <input />
+              <input name='name' onChange={handleChange} />
               <p>Email :</p>
-              <input />
+              <input name='email' onChange={handleChange} />
               <p>Password :</p>
-              <input />
+              <input name='password' onChange={handleChange} />
               <p>Confirm Password :</p>
-              <input />
-              <button>Login</button>
-          </div>
+              <input name='cpassword' onChange={handleChange} />
+              <button type='submit'>Login</button>
+          </form>
       </div>
     </>
   )
