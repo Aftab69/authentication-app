e324120dda02ea613d41edb9ecdf99d273172cb2
Aftab69 <aftabhssn3@gmail.com>
2022-08-17 10:57:41 +0530

    token verification done and user data retrieved

--- backend/Routing.js
+++ backend/Routing.js
@@ -3,9 +3,25 @@ const router = express.Router();
 const User = require("./Model");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+router.use(cookieParser());
 
 router.get("/",(req,res)=>{
     res.send("Hi there")
+})
+
+router.get("/profile",async(req,res)=>{
+    try{
+        const verifiedToken = jwt.verify(req.cookies.jwtoken,process.env.PRIVATEKEY);
+        const userData = await User.findOne({_id:verifiedToken._id})
+        if(userData){
+            res.status(200).send(userData);
+        } else {
+            res.status(400).json({"message":"User unauthorised"})
+        }
+    } catch(error){
+            res.status(400).json({"message":"User unauthorised"})
+    }
 })
 
 router.post("/register",async(req,res)=>{
@@ -48,6 +64,7 @@ router.post("/login",async(req,res)=>{
             const token = jwt.sign({_id:userExist._id},process.env.PRIVATEKEY);
             userExist.tokens.push({token: token});
             userExist.save();
+            res.cookie("jwtoken",token);
             res.status(200).json({"message":"User successfully logged in"})
         }
     }catch(error){
--- frontend/src/Profile.js
+++ frontend/src/Profile.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from "react-router-dom"
 import "./Profile.css";
 
 const Profile = () => {
+  const navigate = useNavigate();
+  const [ visibility, setVisibility ] = useState({display:"none"})
+  const getData = async() =>{
+      try{
+        const res = await fetch("/profile",{
+                    method:"GET",
+                    headers:{
+                      "Content-Type":"application/json"
+                    },
+                    credentials:"include"
+                  })
+        if(res.status===200){
+            const data = await res.json();
+            console.log(data); //data of the user
+            setVisibility({display:"block"})
+        } else if(res.status===400) {
+            navigate("/login")
+        }
+      } catch(error){
+            navigate("/login")
+      }
+  }
+  useEffect(()=>{
+    getData()
+  },[]) // eslint-disable-line react-hooks/exhaustive-deps
+
   return (
-    <div>Profile</div>
+    <>
+      <div style={visibility}>
+        <p>Profile page</p>
+      </div>
+    </>
   )
 }
 
