dff45cbacff5b9f013ca7520d4e8412cc3e15cc3
Aftab69 <aftabhssn3@gmail.com>
2022-08-22 23:04:18 +0530

    deploying to heroku

--- App.js
+++ App.js
@@ -10,6 +10,10 @@ require("./Model");
 //for heroku
 if(process.env.NODE_ENV == "production"){
     app.use(express.static("client/build"));
+    const path = require("path");
+    app.get("*", (req,res)=>{
+        res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+    })
 }
 
 app.listen(process.env.PORT || 5000,(req,res)=>{
