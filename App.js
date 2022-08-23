f0bb97268c092eb818cb5e0ac1eb098a39c6704e
Aftab69 <aftabhssn3@gmail.com>
2022-08-23 10:34:12 +0530

    ready for deploy to heroku

--- App.js
+++ App.js
@@ -7,7 +7,14 @@ require("./Connection");
 app.use(require("./Routing"));
 require("./Model");
 
+if( process.env.NODE_ENV == "production"){
+    app.use(express.static("client/build"));
+    const path = require("path");
+    app.get("*", (req,res)=>{
+        res.sendFile(path.resolve( __dirname, 'client', 'build', 'index.html'))
+    })
+}
 
-app.listen(process.env.PORT,(req,res)=>{
+app.listen(process.env.PORT || 5000,(req,res)=>{
     console.log(`app is running at ${process.env.PORT}`)
 });
