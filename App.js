176febcb93c1415499693e934f8548f433c2631d
Aftab69 <aftabhssn3@gmail.com>
2022-11-27 14:02:57 +0530

    deploying to cyclic

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 app.use(express.json());
 const dotenv = require("dotenv");
 dotenv.config({ path:"./config.env" });
@@ -7,13 +8,22 @@ require("./Connection");
 app.use(require("./Routing"));
 require("./Model");
 
-if( process.env.NODE_ENV == "production"){
-    app.use(express.static("client/build"));
-    const path = require("path");
-    app.get("*", (req,res)=>{
-        res.sendFile(path.resolve( __dirname, 'client', 'build', 'index.html'))
-    })
-}
+// if( process.env.NODE_ENV == "production"){
+//     app.use(express.static("client/build"));
+//     const path = require("path");
+//     app.get("*", (req,res)=>{
+//         res.sendFile(path.resolve( __dirname, 'client', 'build', 'index.html'))
+//     })
+// }
+app.use(express.static(path.join(__dirname, "./client/build")));
+app.get("*", function (_, res) {
+  res.sendFile(
+    path.join(__dirname, "./client/build/index.html"),
+    function (err) {
+      res.status(500).send(err);
+    }
+  );
+});
 
 app.listen(process.env.PORT || 5000,(req,res)=>{
     console.log(`app is running at ${process.env.PORT}`)
